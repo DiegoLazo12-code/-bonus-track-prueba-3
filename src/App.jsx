bc145057bc0591d8746739bf54577eed59bbb330
Diego Lazo <diego.lazo06@inacapmail.cl>
2026-06-26 01:49:38 -0400

    Cuarto commit: feat: renderizado de la lista con key

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import { mascotas } from '../data/mascota.js'
 import ListaMascotas from './components/ListaMascotas.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log('mascotas cargadas:', mascotas)
-
   return (
-    <>
-      <section id="mascotas-section">
-        <h2>Lista de mascotas</h2>
-        <ListaMascotas mascotas={mascotas} />
-      </section>
-
-      <div className="ticks"></div>
-
-      
-    </>
+    <section id="mascotas-section">
+      <h2>Lista de mascotas</h2>
+      <ListaMascotas mascotas={mascotas} />
+    </section>
   )
 }
 
